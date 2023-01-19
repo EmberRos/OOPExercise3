38b1e5f45fe62211399dd385a7fe21af8c87be7b
Embear <117084390+EmberRos@users.noreply.github.com>
2023-01-19 16:09:15 +0800

    Add files via upload

---
+++ CarEngine.java
@@ -0,0 +1,10 @@
+class CarEngine {
+    public String startCarEngine(){
+        return" The Car Engine has started ";
+    }
+    public String stopRocketEngine(){
+        return" The Car Engine has stopped";
+    }
+
+}
+
---
+++ Main.java
@@ -0,0 +1,9 @@
+
+public class Main {
+    public static void main(String[] args){
+        Car nasa = new Car(" White ");
+        System.out.println(nasa.start());
+    }
+
+}
+
